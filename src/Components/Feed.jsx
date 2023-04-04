c88e5d37931729ed0ebcc0fbaec7f447f1f7d6e5
JassiSingh08 <singhjassi693@gmail.com>
2023-04-04 16:31:49 +0530

    c

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -5,7 +5,7 @@ import { Box, Stack, Typography } from '@mui/material';
 const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>      
+      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}> 
       {/* md means medium devices sx means if laptop keep same if md change accordingly can do with help of objects and material ui  */}
 
         Sidebar
